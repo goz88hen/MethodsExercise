4df300d6b6a7f770c7d81915ad6c83dfdfe4a4f4
Henry (Hendra) Gozali <123764602+goz88hen@users.noreply.github.com>
2023-02-11 03:48:20 -0800

    Began the first step

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -1,10 +1,27 @@
 ﻿namespace MethodsExercise
 {
-    public class Program
+    class program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Exercise 1
+            Console.WriteLine("What is your name?");
+            var userName = Console.ReadLine();
+
+            Console.WriteLine("What is your favorite color?");
+            var color = Console.ReadLine();
+
+            Console.WriteLine("What is your favorite animal?");
+            var animal = Console.ReadLine();
+
+            Console.WriteLine("Who is your favorite band?");
+            var band = Console.ReadLine();
+
+            Console.WriteLine($"Name:{userName}");
+            Console.WriteLine($"Favorite Band: (band)");
+            Console.WriteLine($"Favorite Animal (animal)");
+            Console.WriteLine($"Favorite Color (color)");
         }
     }
 }
+
